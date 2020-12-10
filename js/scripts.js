8d662c3db30acfcf861dcef110056f667dea1404
DawnDesign7 <dawnmoraa@gmail.com>
2020-12-10 23:01:09 +0300

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function bodymass (weight, height) {
+return alert
+}
+var height= Number(prompt ("Enter Height in metres"));
+var weight= Number(prompt("Enter Weight in Kgs"));
+var finalHeight= height*height;
+var bmi = weight / finalHeight;
+
+alert("Your BMI is" + bmi.toFixed(2) );
